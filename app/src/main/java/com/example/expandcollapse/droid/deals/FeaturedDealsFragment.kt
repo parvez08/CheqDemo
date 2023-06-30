ebddc11416717ae248a16bcb5b834a9e96f9b74b
parvez08 <parvezshaikh.0894@gmail.com>
2023-06-30 15:44:47 +0530

    - Created featured deals fragment.
    - Setting up recycler view.
    
    - Relating adapter.

--- app/src/main/java/com/example/expandcollapse/droid/deals/FeaturedDealsFragment.kt
+++ app/src/main/java/com/example/expandcollapse/droid/deals/FeaturedDealsFragment.kt
@@ -5,12 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.expandcollapse.R
 import com.example.expandcollapse.databinding.FragmentFeaturedDealsBinding
+import com.example.expandcollapse.droid.deals.adapter.FeaturedDealsAdapter
+import com.example.expandcollapse.droid.deals.adapter.OnDealSelectedListenerCallback
+import com.example.expandcollapse.droid.deals.models.FeaturedDealsModel
+import com.example.expandcollapse.droid.utils.RecyclerViewUtils
 
 
 class FeaturedDealsFragment : Fragment() {
     private lateinit var binding: FragmentFeaturedDealsBinding
-
+    private var featuredDealsList: ArrayList<FeaturedDealsModel> = arrayListOf()
+    private lateinit var mDealsAdapter: FeaturedDealsAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -25,7 +32,71 @@ class FeaturedDealsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setUpRecyclerView()
+    }
 
+    private fun setUpRecyclerView() {
+        addVoucherCategoryData(
+            R.drawable.swiggy_merch_logo,
+            R.drawable.swiggy_merch_illustration,
+            1000,
+            R.color.cheq_white_a,
+            R.color.cheq_yellow
+        )
+        addVoucherCategoryData(
+            R.drawable.subway_merch_logo,
+            R.drawable.subway_merch_illustration,
+            150,
+            R.color.cheq_white_a,
+            R.color.cheq_green
+        )
+        addVoucherCategoryData(
+            R.drawable.swiggy_merch_logo,
+            R.drawable.swiggy_merch_illustration,
+            2000,
+            R.color.cheq_white_a,
+            R.color.cheq_yellow
+        )
+        addVoucherCategoryData(
+            R.drawable.subway_merch_logo,
+            R.drawable.subway_merch_illustration,
+            250,
+            R.color.cheq_white_a,
+            R.color.cheq_green
+        )
+        mDealsAdapter = FeaturedDealsAdapter(
+            featuredDealsList,
+            requireContext(),
+            object : OnDealSelectedListenerCallback {
+                override fun onSelected(position: Int) {
+
+                }
+
+            })
+        binding.rvFeaturedDeals.apply {
+            adapter = mDealsAdapter
+            layoutManager =
+                LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+            RecyclerViewUtils.maximizeEfficiency(this)
+        }
+    }
+
+    private fun addVoucherCategoryData(
+        merchLogo: Int,
+        merchIllustration: Int,
+        dealPrice: Int,
+        merchLogoBg: Int,
+        merchIllustrationBg: Int
+    ) {
+        featuredDealsList.add(
+            FeaturedDealsModel(
+                merchLogo,
+                merchIllustration,
+                dealPrice,
+                merchLogoBg,
+                merchIllustrationBg
+            )
+        )
     }
 
     companion object {
