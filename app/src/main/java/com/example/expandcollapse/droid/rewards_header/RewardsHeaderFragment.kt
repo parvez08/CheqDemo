866777c660abd5f54744bf29c1c12302fdbabf92
parvez08 <parvezshaikh.0894@gmail.com>
2023-06-30 15:52:23 +0530

    - Setting on click listeners for get instant cast cta.
    - Fragment transaction.

--- app/src/main/java/com/example/expandcollapse/droid/rewards_header/RewardsHeaderFragment.kt
+++ app/src/main/java/com/example/expandcollapse/droid/rewards_header/RewardsHeaderFragment.kt
@@ -1,11 +1,16 @@
 package com.example.expandcollapse.droid.rewards_header
 
+import android.animation.AnimatorInflater
+import android.animation.StateListAnimator
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.expandcollapse.R
 import com.example.expandcollapse.databinding.FragmentRewardsHeaderBinding
+import com.example.expandcollapse.droid.BlankFragment
+import com.example.expandcollapse.droid.explore_vouchers.ExploreVouchersFragment
 
 
 class RewardsHeaderFragment : Fragment() {
@@ -25,6 +30,29 @@ class RewardsHeaderFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setCta()
+    }
+
+    private fun setCta() {
+        binding.clCtaGetCashInstant.setOnLongClickListener {
+            val stateListAnimator: StateListAnimator =
+                AnimatorInflater.loadStateListAnimator(
+                    context,
+                    R.animator.selector_item_animation
+                )
+            binding.clCtaGetCashInstant.stateListAnimator = stateListAnimator
+            true
+        }
+        binding.clCtaGetCashInstant.setOnClickListener {
+            activity?.supportFragmentManager?.beginTransaction()
+                ?.replace(
+                    R.id.fl_top_frag,
+                    BlankFragment.newInstance(),
+                    BlankFragment.TAG2
+                )
+                ?.addToBackStack(ExploreVouchersFragment.TAG2)
+                ?.commit()
+        }
 
     }
 
