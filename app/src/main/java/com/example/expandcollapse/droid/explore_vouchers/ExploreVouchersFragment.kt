9e1b701580f2e99cdc19754a0650864bbae894d7
parvez08 <parvezshaikh.0894@gmail.com>
2023-06-30 15:43:50 +0530

    - setting up recyclerview for mini vouchers item.

--- app/src/main/java/com/example/expandcollapse/droid/explore_vouchers/ExploreVouchersFragment.kt
+++ app/src/main/java/com/example/expandcollapse/droid/explore_vouchers/ExploreVouchersFragment.kt
@@ -5,12 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.expandcollapse.R
 import com.example.expandcollapse.databinding.FragmentExploreVouchersBinding
+import com.example.expandcollapse.droid.BlankFragment
+import com.example.expandcollapse.droid.explore_vouchers.adapters.OnSelectedListenerCallback
+import com.example.expandcollapse.droid.explore_vouchers.adapters.VouchersAdapter
+import com.example.expandcollapse.droid.explore_vouchers.models.VouchersCategoryModel
+import com.example.expandcollapse.droid.utils.RecyclerViewUtils
 
 
 class ExploreVouchersFragment : Fragment() {
     private lateinit var binding: FragmentExploreVouchersBinding
-
+    private var vouchersCategoryList: ArrayList<VouchersCategoryModel> = arrayListOf()
+    private lateinit var mVouchersAdapter: VouchersAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -25,7 +33,42 @@ class ExploreVouchersFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setUpRecyclerView()
+    }
 
+    private fun setUpRecyclerView() {
+        addVoucherCategoryData("Food", R.drawable.food)
+        addVoucherCategoryData("Travel", R.drawable.travel)
+        addVoucherCategoryData("Shopping", R.drawable.shopping)
+        mVouchersAdapter = VouchersAdapter(vouchersCategoryList, requireContext(), object :
+            OnSelectedListenerCallback {
+            override fun onSelected(position: Int) {
+                activity?.supportFragmentManager?.beginTransaction()
+                    ?.replace(
+                        R.id.fl_top_frag,
+                        BlankFragment.newInstance(),
+                        BlankFragment.TAG2
+                    )
+                    ?.addToBackStack(TAG2)
+                    ?.commit()
+            }
+        })
+        mVouchersAdapter.addItemAtLast(VouchersCategoryModel("See All", R.drawable.see_all))
+        binding.rvVouchersCategory.apply {
+            adapter = mVouchersAdapter
+            layoutManager =
+                LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+            RecyclerViewUtils.maximizeEfficiency(this)
+        }
+    }
+
+    private fun addVoucherCategoryData(categoryName: String, categoryDrawable: Int) {
+        vouchersCategoryList.add(
+            VouchersCategoryModel(
+                categoryName,
+                categoryDrawable
+            )
+        )
     }
 
     companion object {
